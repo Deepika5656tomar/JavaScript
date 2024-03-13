ac8c6fc7e4c5a4ada92c5d9ec053aae105226478
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-13 15:23:55 +0000

    conversion

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,2 +1,14 @@
-console.log(typeof undefined);
-console.log(typeof null);
+//let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+ //let value=Number(score)
+ //console.log(typeof value);
+ //console.log(value);
+let isLoggedIn=""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let number=12
+let stringNumber=String(number)
+console.log(stringNumber);
