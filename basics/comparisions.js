58feb8a8d9bfdb4d28ad3b7cc8cdee2dbe5057cc
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-13 16:26:47 +0000

    datatype summary

--- basics/comparisions.js
+++ basics/comparisions.js
@@ -1,4 +1,4 @@
-console.log("2">1);
+/*console.log("2">1);
 console.log("02">1);
 console.log("2"===2);
 console.log(null >0);
@@ -6,4 +6,27 @@ console.log(null == 0);
 console.log(null >= 0);
 console.log(undefined == 0);
 console.log(undefined >0);
-console.log(undefined <0);
+console.log(undefined <0);*/
+
+//****summary ****//
+
+//primitive 
+
+// 7 types : string,number,boolean,null,undefined,symbol,BigInt
+
+// reference type(non primitive)
+// array,objects,functions
+
+const id=Symbol('123')
+const anotherId =Symbol('123')
+console.log(id===anotherId);
+
+
+const heros=["shaktimaan","naagaaj","doga"];
+let myObj = {
+    name: "Deepika",
+    age: 21,
+}
+const myFunction = function(){
+    console.log("hello world");
+}
