f5dfe1a89807e39bc5395431fc66ea923202088c
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-13 15:38:36 +0000

    operations

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -5,10 +5,27 @@
  //let value=Number(score)
  //console.log(typeof value);
  //console.log(value);
-let isLoggedIn=""
+/*let isLoggedIn=""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 
 let number=12
 let stringNumber=String(number)
-console.log(stringNumber);
+console.log(stringNumber);*/
+
+//**********operations *********//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1="hello"
+let str2=" deepika"
+
+let str3= str1+str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+"3"+"3");
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,4 @@
-console.log("deepika")
+const accountId = 144553
+let accountPassword = "21211"
+accountCity = "pkw"
+console.table([accountId, accountPassword, accountCity]);
