91bf7c0e613d6a0d8cf1b2eda72cb6af2e2ffb22
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-15 17:58:07 +0000

    string

--- basics/strings.js
+++ basics/strings.js
@@ -3,3 +3,6 @@ const count= 100
 
 console.log(`hello my name is ${name} and count is ${count}`);//string interpolation
 const gameName = new String("deepika");
+a=gameName.charAt();
+console.log(a);
+console.log(gameName);
