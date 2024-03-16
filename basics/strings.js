16dba018a950e497f768778c5b2d42aedfc5d303
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-16 14:16:03 +0000

    strings done

--- basics/strings.js
+++ basics/strings.js
@@ -1,8 +1,26 @@
-const name="deepika"
+const name="deepika-t"
 const count= 100
 
 console.log(`hello my name is ${name} and count is ${count}`);//string interpolation
-const gameName = new String("deepika");
+const gameName = new String("d-e-e-pika");
 a=gameName.charAt();
-console.log(a);
+/*console.log(a);
 console.log(gameName);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('e'));
+//console.log();
+const newString = gameName.substring(0,4)
+console.log(newString);
+const s1= gameName.substring(-5,4)
+console.log(s1);
+const s2="  deepika "
+console.log(s2.trimStart());
+const url="https://google.com/google%20search"
+console.log(url.replace('%20',"-"));
+console.log(url.includes('deepika'));
+console.log(gameName.split('-'));*/
+console.log(gameName.substr(0,4));
+console.log(gameName.repeat(4));
