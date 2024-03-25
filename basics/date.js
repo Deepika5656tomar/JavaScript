a596910b9c67b41812422de1e90ff2e9a39e3474
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-25 17:41:38 +0000

    date and time

---
+++ basics/date.js
@@ -0,0 +1,21 @@
+// dates
+ let myDate = new Date()
+ console.log(myDate);
+ console.log(myDate.toDateString());
+ console.log(myDate.toLocaleString());
+ console.log(typeof myDate);
+
+ let newDate = new Date(2024,3,25)
+ console.log(newDate);
+ console.log(newDate.toLocaleString());
+let d= new Date("2023-01-24")
+ console.log(d);
+ console.log(d.toLocaleString());
+
+ let t = Date.now()
+ console.log(t);
+ console.log(d.getTime());
+
+ let v=new Date()
+ console.log(v);
+ console.log(v.getFullYear());
