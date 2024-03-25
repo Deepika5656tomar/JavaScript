83d0bf5393c1347ce09229eb9cba178ec684e4c9
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-25 17:55:35 +0000

    array

---
+++ basics2/array.js
@@ -0,0 +1,8 @@
+let book=["maths","bio","english"]
+console.log(book[0]);
+book.push("hindi");   //pop also
+book.unshift(9);
+console.log(book);
+console.log(book.shift());
+console.log(book.includes(9));
+console.log(book.indexOf("maths"));
