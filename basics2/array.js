2aa83907d5ddb1c278ecc56b20afd1ca3a0f8ee0
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-26 04:40:12 +0000

    arrays

--- basics2/array.js
+++ basics2/array.js
@@ -1,4 +1,4 @@
-let book=["maths","bio","english"]
+/*let book=["maths","bio","english"]
 console.log(book[0]);
 book.push("hindi");   //pop also
 book.unshift(9);
@@ -6,3 +6,34 @@ console.log(book);
 console.log(book.shift());
 console.log(book.includes(9));
 console.log(book.indexOf("maths"));
+
+let myArr=[1,2,3,4,5,6];
+const p=myArr.slice(1,3)
+
+console.log(p);
+console.log(myArr);
+
+const q=myArr.splice(1,3)
+console.log(q);
+console.log(myArr);
+
+let book=["maths","bio","english"]
+let value=[1,2,3,4,5]
+//book.push(value);
+console.log(book);
+const a=book.concat(value)
+console.log(a);
+const v=[...book, ...value]
+console.log(v);
+
+Arr=[1,2,3,4,[5,6,7,[8,9,[10,11,[0,2]]]]]
+const myArr=Arr.flat(2)
+console.log(myArr);
+console.log(Array.isArray("Deepika"))
+console.log(Array.from("12345"))*/
+
+let s1=10
+let s2=20
+let s3=30
+
+console.log(Array.of(s1,s2,s3));
