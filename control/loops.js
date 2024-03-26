1348ede0acf02282b98c152a378c3ed8ea922cf3
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-26 18:05:26 +0000

    while loop

--- control/loops.js
+++ control/loops.js
@@ -1,14 +1,32 @@
-for (let index = 0; index < 10; index++) {
-    const element = index;
-    console.log(element);
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
     
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`outer loop ${i}`);
+//     for (let j = 0; j < 5; j++) {
+//         console.log(`inner loop ${j}`);
+        
+//     }
+    
+// }
+
+// while (condition) {
+    
+// }
+
+let index=0
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index=index+2
 }
 
-for (let i = 0; i < 10; i++) {
-    console.log(`outer loop ${i}`);
-    for (let j = 0; j < 5; j++) {
-        console.log(`inner loop ${j}`);
-        
-    }
-    
+let score=1
+
+do{
+    console.log(`score is ${score}`);
+    score++;
 }
+while(score<=10);
