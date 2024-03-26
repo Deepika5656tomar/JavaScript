3284b9092858a037a74af5c4549d7623b5e31553
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-26 18:56:34 +0000

    loops

--- control/loops2.js
+++ control/loops2.js
@@ -35,7 +35,7 @@
 //     console.log(`${key} for ${myObj[key]}`);
 // }
 
-const arr=["js","python","cpp"]
+
 
 // arr.forEach(function(val){
 //     console.log(val);
@@ -56,21 +56,62 @@ const arr=["js","python","cpp"]
 // })
 
 
-const myCode=[
-    {
-        lname:"javaScript",
-        lfname:"js"
-    },
-    {
-        lname:"java",
-        lfname:"java"
-    },
-    {
-        lname:"python",
-        lfname:"py"
-    }
-]
+// const myCode=[
+//     {
+//         lname:"javaScript",
+//         lfname:"js"
+//     },
+//     {
+//         lname:"java",
+//         lfname:"java"
+//     },
+//     {
+//         lname:"python",
+//         lfname:"py"
+//     }
+// ]
 
-myCode.forEach((item)=>{
-    console.log(item.lname);
-})
+// myCode.forEach((item)=>{
+//     console.log(item.lname);
+// })
+// const arr=["js","python","cpp"]
+
+// const val=arr.forEach((item)=>{
+//     console.log(item);
+// })
+
+// console.log(val);
+
+const nums=[1,2,3,4,5,6,7,8,9]
+
+// let newNums=nums.filter((num)=>num>4)
+// console.log(newNums);
+
+// const newNums=[]
+
+// nums.forEach((num)=>{
+//     if(num>4){
+//         newNums.push(num)
+//     }
+// })
+
+// console.log(newNums);
+
+// const books=[
+//     {
+//         title:'book1',genre:'fiction',edition:2003
+//     },
+//     {
+//         title:'book2',genre:'history',edition:2003
+//     },
+//     {
+//         title:'book1',genre:'non-fiction',edition:2003
+//     },
+//     {
+//         title:'book1',genre:'science',edition:2003
+//     }
+// ]
+// const userbooks=books.filter((bk)=>bk.genre==='history')
+
+// console.log(userbooks);
+
