4937d3eb9d7ee86c4dd3a645702dba6b4776b043
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-26 17:06:51 +0000

    switch and conditional statements

---
+++ control/conditional.js
@@ -0,0 +1,13 @@
+let score = 200
+if(score<100){
+    console.log("less than 100");
+}
+else if(score<200){
+    console.log("less than 200");
+}
+else if(score<300){
+    console.log("less than 300");
+}
+else{
+    console.log("more than 300");
+}
