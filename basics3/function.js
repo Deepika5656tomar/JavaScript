ac2ae50c569e439fba05fcb2e8f863f5bb207838
Deepika <104566303+Deepika5656tomar@users.noreply.github.com>
2024-03-26 12:18:24 +0000

    function1

--- basics3/function.js
+++ basics3/function.js
@@ -17,3 +17,32 @@ function logInUSer(username){
 }
 
  console.log(logInUSer("deepika"))
+
+ function price(...num){      //if in parameter val1,val2,...num will pass then it'll take 200 as val1 and 300 as val2 and remaining in num.
+    return num 
+ }
+
+ console.log(price(200,300,400));
+
+const user={
+    username:"Deepika",
+    age:21
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username}`);
+}
+
+//handleObject(user)
+handleObject({
+    username:"abc",
+    age:22
+})
+
+const myArray=[1,2,3,4,5]
+
+function returnSecondvalue(getArray){
+    return getArray[1]
+}
+//console.log(returnSecondvalue(myArray));
+console.log(returnSecondvalue([3,4,6]));
